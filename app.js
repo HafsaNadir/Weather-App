ee027b370e30424900de34a6b49b929e3cf0e88c
Hafsa Nadir <hafsanadir21@gmail.com>
2019-10-02 02:04:57 +0500

    importing functions in main file and getting input from user

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const request = require('request')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide the address')
+}
+else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log('Error', error)
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log('Error', error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+
+}
+
+
+
 
 // const url = "https://api.darksky.net/forecast/e5bf43507f6aadc19f2af764de5bcc53/37.8267,-122.4233"
 
@@ -16,19 +40,19 @@ const request = require('request')
 //     }
 // })
 
-const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGFmc2EtbmFkaXIyMSIsImEiOiJjanVsbXFlaG0yMjM3NDRxanV3ODl0MGN1In0.RiximN9OsRv3c8AR052ARg"
+// const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGFmc2EtbmFkaXIyMSIsImEiOiJjanVsbXFlaG0yMjM3NDRxanV3ODl0MGN1In0.RiximN9OsRv3c8AR052ARg"
 
-request({url: url, json: true} ,(error,response) => {
-        if(error){
-            console.log('Unable to connect to the weather services')
-        }
-        else if (response.body.error){
-            console.log('Location not found, TRY AGAIN!!')
-        }
-        else {
-            console.log('latitude:' + response.body.features[0].center[0])
-            console.log('longitude:' + response.body.features[0].center[1])
-            console.log('location:' + response.body.features[0].place_name)
-        }
-})
+// request({url: url, json: true} ,(error,response) => {
+//         if(error){
+//             console.log('Unable to connect to the weather services')
+//         }
+//         else if (response.body.error){
+//             console.log('Location not found, TRY AGAIN!!')
+//         }
+//         else {
+//             console.log('latitude:' + response.body.features[0].center[0])
+//             console.log('longitude:' + response.body.features[0].center[1])
+//             console.log('location:' + response.body.features[0].place_name)
+//         }
+// })
 
